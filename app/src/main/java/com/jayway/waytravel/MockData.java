3305bf65c530a7ede94528adfd724ea51b5e27b6
Carl-Emil Kjellstrand <erbsman@gmail.com>
2015-10-02 11:42:57 +0200

    better cordinates

--- app/src/main/java/com/jayway/waytravel/MockData.java
+++ app/src/main/java/com/jayway/waytravel/MockData.java
@@ -9,20 +9,20 @@ public class MockData {
             "        {\n" +
             "            \"title\": \"t1\",\n" +
             "            \"description\": \"d1\",\n" +
-            "            \"longitude\": \"1\",\n" +
-            "            \"latitude\": \"1\"\n" +
+            "            \"longitude\": \"13.0333\",\n" +
+            "            \"latitude\": \"55.5833\"\n" +
             "        },\n" +
             "        {\n" +
             "            \"title\": \"t2\",\n" +
             "            \"description\": \"d2\",\n" +
-            "            \"longitude\": \"1\",\n" +
-            "            \"latitude\": \"1\"\n" +
+            "            \"longitude\": \"12.0333\",\n" +
+            "            \"latitude\": \"55.5833\"\n" +
             "        },\n" +
             "        {\n" +
             "            \"title\": \"t3\",\n" +
             "            \"description\": \"d3\",\n" +
-            "            \"longitude\": \"1\",\n" +
-            "            \"latitude\": \"1\"\n" +
+            "            \"longitude\": \"13.0333\",\n" +
+            "            \"latitude\": \"54.5833\"\n" +
             "        }\n" +
             "    ]\n" +
             "}";
