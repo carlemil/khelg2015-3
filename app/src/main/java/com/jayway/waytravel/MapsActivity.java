952c49f68151a4b3f71f86860d4e79671918f3e7
Simon Zettervall <simon.zettervall@gmail.com>
2015-10-02 15:06:21 +0200

    .

--- app/src/main/java/com/jayway/waytravel/MapsActivity.java
+++ app/src/main/java/com/jayway/waytravel/MapsActivity.java
@@ -24,135 +24,174 @@ import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-	Gson gson = new Gson();
-	private GoogleMap mMap;
-	private       double           longitude        = Double.NaN;
-	private       double           latitude         = Double.NaN;
-	private LocationManager mLm;
-	private final LocationListener locationListener = new LocationListener() {
-		@Override
-		public void onLocationChanged(Location location) {
-			double longitude = location.getLongitude();
-			double latitude = location.getLatitude();
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    Gson gson = new Gson();
+    private GoogleMap mMap;
+    private LocationManager mLm;
 
-			Log.d("TAG", "saker");
+    private boolean mHasCentered;
 
-			LatLng center;
-			center = new LatLng(latitude, longitude);
-			mMap.moveCamera(CameraUpdateFactory.newLatLng(center));
-			mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-			mLm.removeUpdates(this);
-		}
 
-		@Override
-		public void onStatusChanged(String s, int i, Bundle bundle) {
-		}
+    private double mLongitude;
+    private double mLatitude;
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            mLongitude = location.getLongitude();
+            mLatitude = location.getLatitude();
 
-		@Override
-		public void onProviderEnabled(String s) {
-		}
+            Log.d("TAG", "saker");
 
-		@Override
-		public void onProviderDisabled(String s) {
-		}
-	};
+            if (!mHasCentered) {
+                mHasCentered = true;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+                LatLng center;
+                center = new LatLng(mLatitude, mLongitude);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(center));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+            }
 
-		mLm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            invokePostJob();
+        }
 
-		setContentView(R.layout.activity_maps);
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
-	}
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
 
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
-	@Override
-	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
+        @Override
+        public void onProviderEnabled(String s) {
+        }
 
-		mLm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
 
-		final android.os.Handler handler = new android.os.Handler();
-		handler.post(createJob(handler));
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@NonNull
-	private Runnable createJob(final Handler handler) {
-		return new Runnable() {
-			@Override
-			public void run() {
-				OkHttpClient client = new OkHttpClient();
+        mLm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-				final Request request = new Request.Builder().addHeader("Accept", "application/json").url("http://travelway-server.herokuapp.com/users").build();
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-				client.newCall(request).enqueue(new Callback() {
-					@Override
-					public void onFailure(Request request, IOException e) {
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-					}
+        mLm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
 
-					@Override
-					public void onResponse(final Response response) throws IOException {
+        final android.os.Handler handler = new android.os.Handler();
+        handler.post(invokeGetJob(handler));
+    }
 
-						handler.post(new Runnable() {
-							@Override
-							public void run() {
-								PinsDTO persons = null;
-								try {
-									persons = gson.fromJson("{\"pins\":" + response.body().string() + "}", PinsDTO.class);
-								}
-								catch (IOException e) {
-									e.printStackTrace();
-								}
+    @NonNull
+    private Runnable invokePostJob() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = RequestBody.create(JSON, "{\n" +
+                        "\"latitude\": \"" + mLatitude + "\",\n" +
+                        "\"longitude\": \"" + mLongitude + "\"\n" +
+                        "}");
+                Request request = new Request.Builder()
+                        .url("http://travelway-server.herokuapp.com/travellers/0")
+                        .post(body)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
 
-								mMap.clear();
+                    }
 
-								boolean flag = false;
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                    }
+                });
+            }
+        };
+    }
 
-								for (PinDTO p : persons.pins) {
-									LatLng latlan = new LatLng(p.latitude, p.longitude);
 
-									MarkerOptions icon;
+    public Runnable invokeGetJob(final Handler handler) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
 
-									if (!flag) {
-										flag = true;
+                final Request request = new Request.Builder().addHeader("Accept", "application/json").url("http://travelway-server.herokuapp.com/travellers").build();
 
-										icon = new MarkerOptions().position(latlan).title(p.name).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
-									}
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
 
-									else {
-										icon = new MarkerOptions().position(latlan).title(p.name).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-									}
+                    }
 
-									mMap.addMarker(icon);
-								}
+                    @Override
+                    public void onResponse(final Response response) throws IOException {
 
-								handler.postDelayed(createJob(handler), 10 * 1000);
-							}
-						});
-					}
-				});
-			}
-		};
-	}
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                PinsDTO persons = null;
+                                try {
+                                    persons = gson.fromJson("{\"pins\":" + response.body().string() + "}", PinsDTO.class);
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                mMap.clear();
+
+                                boolean flag = false;
+
+                                for (PinDTO p : persons.pins) {
+                                    LatLng latlan = new LatLng(p.latitude, p.longitude);
+
+                                    MarkerOptions icon;
+
+                                    if (!flag) {
+                                        flag = true;
+
+                                        icon = new MarkerOptions().position(latlan).title(p.name).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                                    } else {
+                                        icon = new MarkerOptions().position(latlan).title(p.name).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                                    }
+
+                                    mMap.addMarker(icon);
+                                }
+
+                                handler.postDelayed(invokeGetJob(handler), 10 * 1000);
+                            }
+                        });
+                    }
+                });
+            }
+        };
+    }
 }
+
+
+
 
 
